8905a1a9a53ee3799c8a82f381b5346beee855c6
espkawakeiya-web <espkawakeiya@gmail.com>
2026-05-11 10:26:59 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -2,7 +2,7 @@
  * Name as used as title for the main page
  * @type {string}
  */
-var title = 'SurVis Literature Collection';
+var title = 'Artificial Immune System-Based Intrusion Detection for Secure IoT Environments';
 
 /**
  * Relative path to the data directory
